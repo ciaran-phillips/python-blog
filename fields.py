1b368ee42ddd6eb59d2c4750dd14a396832da80e
Ciaran Phillips <ciaranphillips1@gmail.com>
2013-10-12 16:23:35 +0100

    get_form_field function returns a web.py form input for Field type

--- fields.py
+++ fields.py
@@ -1,5 +1,16 @@
+from web import form
 
 class Field:
-	def __init__(self, name, value):
+	def __init__(self, name, value=None, field_type="Textbox", label=None):
+		if not label:
+			self.label = name.title()
+		else:
+			self.label = label
 		self.name = name
 		self.value = value
+		self.field_type = field_type
+
+	def get_form_field(self):
+		field_class = getattr(form, self.field_type)
+		form_field = field_class(self.name, value=self.value, description = self.label )
+		return form_field
